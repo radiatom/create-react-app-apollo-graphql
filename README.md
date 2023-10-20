e02a4550b620f0dc6a1eadcfaa9db7000758be91
radiatom <arssenmozol@gmail.com>
2023-10-20 10:23:13 +0300

    added server

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
  site is live at https://radiatom.github.io/test-task-list/
+ 
+ tutorial run server: https://www.youtube.com/watch?v=vlDhgLk5m84
---
+++ db.js
@@ -0,0 +1,22 @@
+module.exports = {
+    // posts: [
+    //     { id: 1, title: "Lorem Ipsum", views: 254, user_id: 123 },
+    //     { id: 2, title: "Sic Dolor amet", views: 65, user_id: 456 },
+    // ],
+    // users: [
+    //     { id: 123, name: "John Doe" },
+    //     { id: 456, name: "Jane Doe" }
+    // ],
+    // comments: [
+    //     { id: 987, post_id: 1, body: "Consectetur adipiscing elit", date: new Date('2017-07-03') },
+    //     { id: 995, post_id: 1, body: "Nam molestie pellentesque dui", date: new Date('2017-08-17') }
+    // ]
+    todos: [
+        { id: 1, title: "Lorem Ipsum", user_id: 123, completed: false },
+        { id: 2, title: "Sic Dolor amet", user_id: 456, completed: true },
+    ],
+    users: [
+        { id: 123, name: "John Doe" },
+        { id: 456, name: "Jane Doe" }
+    ],
+}
